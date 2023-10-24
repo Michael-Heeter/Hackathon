b4216c01766b12d50a4f12319c4d99b8212c0dac
Michael Heeter <137200885+CheaTeR13@users.noreply.github.com>
2023-10-24 16:37:43 -0400

    Final for Presentation

--- main.js
+++ main.js
@@ -1,39 +1,23 @@
-const aChart = document.getElementById('myChart').getContext('2d')
+const aChart = document.getElementById('myChart').getContext('2d');
 
-let gradient = aChart.createLinearGradient(0,0,0,400)
-gradient.addColorStop(0, 'rgba(58,123,213,1')
-gradient.addColorStop(1, 'rgba(0,210,255,0.3')
 
 const data = {
-  labels: ['Japan', 'Ukraine', 'Germany', 'UK','Italy','France','Brazil','Russia','China','USA'],
+  labels: ['Japan', 'Ukraine', 'Germany', 'UK','Italy','France','Brazil','Russia','China','USA'].reverse(),
   datasets: [{
     label: 'Population of Cats',
-    data: [7.25, 7.5, 7.75,7.75,9.5,9.5,12.5,12.75,53,76.5],
-    fill: true,
-    backgroundColor: gradient,
-    borderColor: 'grey',
-    pointBackgroundColor: 'red',
-    borderWidth: 1.5,
+    data: [7250000, 7500000, 7750000,7750000,9500000,9500000,12500000,12750000,53000000,76500000].reverse(),
+    backgroundColor: ['rgba(255, 99, 132, 0.5)','rgba(99,255,132,0.5)','rgba(132,255,255,0.5)','rgba(255,132,255,0.5)','rgba(132,99,255,0.5)','rgba(255,255,99,0.5)','rgba(255,99,255,0.5)','rgba(255,165,0,0.5)','rgba(0,128,0,0.5)','rgba(128,0,128,0.5)'],
+    borderWidth: 0,
     borderJoinStyle: 'miter'
   }]
 };
 
 const myChart = new Chart(aChart, {
-  type: 'line',
+  type: 'pie',
   data: data,
   options: {
-    radius: 5,
-    hitRadius: 60,
+
     responsive: true,
-    scales:{
-        y:{
-            ticks:{
-                callback: function(value){
-                    return value + ' million';
-                }
-            }
-        }
-    },
     plugins:{
         legend:{
             labels:{
